b3ad71b1db1837e00958e0ec388a80cafb49cefe
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2023-06-21 19:35:44 +0300

    pass a function as a prop & render a list of elements

--- src/components/hello-message/hello-message.tsx
+++ src/components/hello-message/hello-message.tsx
@@ -1,22 +1,30 @@
-import {component$} from '@builder.io/qwik';
+import { component$, PropFunction } from "@builder.io/qwik";
 
 interface HelloMessageProps {
-    message: string,
-    courseVersion?: number
+  message: string,
+  courseVersion?: number,
+  showButton: boolean,
+  onShowMessage: PropFunction<(message: string) => void>;
 }
 
 
 export const HelloMessage = component$((props: HelloMessageProps) => {
-    const {message, courseVersion} = props;
+  const { message, courseVersion, onShowMessage, showButton } = props;
 
-    return (
-        <div class="container">
-            {
-                <h1>
-                    {message} - version: {courseVersion}
-                </h1>
+  return (
+    <div class="container">
+      {
+        <>
+          <h1>
+            {message} - version: {courseVersion}
+          </h1>
 
-            }
-        </div>
-    );
+          {showButton && (
+            <button onClick$={() => onShowMessage(message)}>Show Message</button>
+          )}
+        </>
+
+      }
+    </div>
+  );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,33 @@
-import {component$} from '@builder.io/qwik';
+import {component$, $} from '@builder.io/qwik';
 import {HelloMessage} from '~/components/hello-message/hello-message';
 
 
 export default component$(() => {
 
+
+
     console.log('Initializing HelloWorld Component');
+
+    const sayHello = $(() => {
+      alert('Hello World');
+    });
+
+    const messages = [
+      'Hello World',
+      'Hello World 2',
+      'Hello World 3'
+    ];
+
+    const onShowMessageClicked = $((message: string) => {
+      alert(message);
+    });
+
     return (
         <>
-            <HelloMessage message="Hello World" courseVersion={1} />
-            <HelloMessage message="Hello World 2" courseVersion={2}/>
-            <HelloMessage message="Hello World 3"/>
+          { messages.map((message, index) => (
+            <HelloMessage key={index} message={message} showButton={true} onShowMessage={onShowMessageClicked} />
+          ))}
+            <button onClick$={sayHello}>Say Hello!</button>
         </>
     );
 });
